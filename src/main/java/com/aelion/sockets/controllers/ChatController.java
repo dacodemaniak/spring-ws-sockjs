d389887755aad4751a7d31427fb6dab500cf9fae
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-01-06 16:58:42 +0100

    Add Controller annotation

--- src/main/java/com/aelion/sockets/controllers/ChatController.java
+++ src/main/java/com/aelion/sockets/controllers/ChatController.java
@@ -4,14 +4,17 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
 
 import com.aelion.sockets.models.ChatMessage;
 
+@Controller
 public class ChatController {
     @MessageMapping("/chat.register")
     @SendTo("/topic/public")
     public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        System.out.println("Received a new user : " + chatMessage.getSender());
         return chatMessage;
     }
 
